5e2e2138d3a928c1755dc8250a2c526ddcc9263c
DESKTOP-9T5OK38\dosss <dosss2726@gmail.com>
2024-01-10 22:48:23 +0900

    삼각형 그리기 구현

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,76 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <fstream>
+#include <string>
 #include <iostream>
+using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+int setWindow();
+unsigned int setShader(string vertexShaderSource, GLenum shaderType);
+void setProgram(unsigned int vertexShader, unsigned int fragShader);
+string readFile(string filePath);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-//
+
+GLFWwindow * window;
+
 int main()
 {
+    if(setWindow() == -1){
+        return -1;
+    }
 
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+    unsigned int vertexShader = setShader("./shader/simple.vs", GL_VERTEX_SHADER);
+    unsigned int fragmentShader = setShader("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    setProgram(vertexShader, fragmentShader);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    glEnableVertexAttribArray(0);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+int setWindow(){
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -26,7 +84,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -42,31 +100,52 @@ int main()
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }    
-
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-        // input
-        // -----
-        processInput(window);
-
-        // render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
     }
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
     return 0;
+}
+
+string readFile(string filePath){
+    ifstream infile(filePath);
+
+    // 파일에서 문자열을 읽습니다.
+    std::string str = "";
+    string temp;
+    while(getline(infile, temp)){
+			str += temp;
+            str += "\n";
+		}
+    // 문자열을 출력합니다.
+    std::cout << str.c_str() << std::endl;
+        return str;
+}
+
+unsigned int setShader(string shaderSource, GLenum shaderType){
+    unsigned int shader;
+    shader = glCreateShader(shaderType);
+    std::string shaderCode = readFile(shaderSource);
+    GLchar const * temp[] = {shaderCode.c_str()};
+    glShaderSource(shader, 1, temp, NULL);
+    glCompileShader(shader);
+
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << endl;
+    }
+    return shader;
+}
+
+void setProgram(unsigned int vertexShader, unsigned int fragShader){
+    unsigned int program;
+    program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragShader);
+    glLinkProgram(program);
+    glUseProgram(program);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragShader);
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
@@ -81,6 +160,7 @@ void processInput(GLFWwindow *window)
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+    std::cout << 123 << std::endl;
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
