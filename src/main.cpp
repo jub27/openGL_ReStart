6d3451defd3463fb82c7429e387d7d777991718c
JaeyeopKim <jykim@neofect.com>
2024-01-10 11:10:23 +0900

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,10 @@ void processInput(GLFWwindow *window);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
+//
 int main()
 {
+
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
