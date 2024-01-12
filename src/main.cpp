dcb3f40eeedd1111587f1a6117e9433c91e5e75e
DESKTOP-9T5OK38\dosss <dosss2726@gmail.com>
2024-01-12 22:18:31 +0900

    moel, view, projection 행렬 적용

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 int setWindow();
 unsigned int setShader(string vertexShaderSource, GLenum shaderType);
@@ -24,25 +24,25 @@ string readFile(string filePath);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-GLFWwindow * window;
+GLFWwindow *window;
 
 int main()
 {
-    if(setWindow() == -1){
+    if (setWindow() == -1)
+    {
         return -1;
     }
 
     float vertices[] = {
         // 위치              // 컬러             // 텍스처 좌표
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 우측 상단
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 우측 하단
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 좌측 하단
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 좌측 상단
-    };  
-    unsigned int indices[] = {  // 0부터 시작한다는 것을 명심하세요!
-        0, 1, 3,
-        1, 2, 3
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // 우측 상단
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // 우측 하단
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // 좌측 하단
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // 좌측 상단
     };
+    unsigned int indices[] = {// 0부터 시작한다는 것을 명심하세요!
+                              0, 1, 3,
+                              1, 2, 3};
 
     Shader shaderProgram = Shader("./shader/simple.vs", "./shader/simple.fs");
     shaderProgram.use();
@@ -72,12 +72,14 @@ int main()
     glGenTextures(1, &texture0);
     glBindTexture(GL_TEXTURE_2D, texture0);
     int width, height, nrChannels;
-    unsigned char * data = stbi_load("./image/container.jpg", &width, &height, &nrChannels, 0);
-    if(data){
+    unsigned char *data = stbi_load("./image/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
-    else{
+    else
+    {
         cout << "Failed to load texture" << endl;
     }
     unsigned int texture1;
@@ -86,22 +88,19 @@ int main()
     glBindTexture(GL_TEXTURE_2D, texture1);
     stbi_set_flip_vertically_on_load(true);
     data = stbi_load("./image/awesomeface.png", &width, &height, &nrChannels, 0);
-    if(data){
+    if (data)
+    {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
-    else{
+    else
+    {
         cout << "Failed to load texture" << endl;
     }
     glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture0"), 0);
     glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture1"), 1);
-    
+
     stbi_image_free(data);
-
-
-    
-    
-    
 
     // render loop
     // -----------0
@@ -115,12 +114,17 @@ int main()
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, 0.0f, 0.0f));
-        trans = glm::scale(trans, glm::vec3(0.5f, 1.0f, 1.0f));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0, 0, 1.0f));
-        
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "transform"),1, GL_FALSE, glm::value_ptr(trans));
+
+        glm::mat4 model(1.0f);
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        glm::mat4 view(1.0f);
+        view = glm::translate(view, glm::vec3(0, 0, -3.0f));
+        glm::mat4 projection(1.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / SCR_HEIGHT, 0.1f, 100.0f);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)(sizeof(float) * 0));
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -134,7 +138,8 @@ int main()
     return 0;
 }
 
-int setWindow(){
+int setWindow()
+{
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -168,38 +173,44 @@ int setWindow(){
     return 0;
 }
 
-string readFile(string filePath){
+string readFile(string filePath)
+{
     ifstream infile(filePath);
 
     // 파일에서 문자열을 읽습니다.
     std::string str = "";
     string temp;
-    while(getline(infile, temp)){
-			str += temp;
-            str += "\n";
+    while (getline(infile, temp))
+    {
+        str += temp;
+        str += "\n";
     }
     return str;
 }
 
-unsigned int setShader(string shaderSource, GLenum shaderType){
+unsigned int setShader(string shaderSource, GLenum shaderType)
+{
     unsigned int shader;
     shader = glCreateShader(shaderType);
     std::string shaderCode = readFile(shaderSource);
-    GLchar const * temp[] = {shaderCode.c_str()};
+    GLchar const *temp[] = {shaderCode.c_str()};
     glShaderSource(shader, 1, temp, NULL);
     glCompileShader(shader);
 
-    int  success;
+    int success;
     char infoLog[512];
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if(!success){
+    if (!success)
+    {
         glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << endl;
+        cout << "ERROR::SHADER::COMPILATION_FAILED\n"
+             << infoLog << endl;
     }
     return shader;
 }
 
-unsigned int setProgram(unsigned int vertexShader, unsigned int fragShader){
+unsigned int setProgram(unsigned int vertexShader, unsigned int fragShader)
+{
     unsigned int program;
     program = glCreateProgram();
     glAttachShader(program, vertexShader);
@@ -215,15 +226,15 @@ unsigned int setProgram(unsigned int vertexShader, unsigned int fragShader){
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
+    // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
