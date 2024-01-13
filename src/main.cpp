0ab2fe48c05bdc5ec4bdf0382a30a1997d943612
DESKTOP-9T5OK38\dosss <dosss2726@gmail.com>
2024-01-13 17:54:05 +0900

    카메라가 target을 기준으로 회전

--- src/main.cpp
+++ src/main.cpp
@@ -160,7 +160,8 @@ int main()
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 13.0f);
+        float radius = 10.0f;
+        glm::vec3 cameraPos = glm::vec3(sin(glfwGetTime()) * radius, 0.0f, cos(glfwGetTime()) * radius);
         glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
         glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
         glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, up);
