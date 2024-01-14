23f7f28248ea0c912e585ccbe7af97b87883ac95
DESKTOP-9T5OK38\dosss <dosss2726@gmail.com>
2024-01-14 11:24:41 +0900

    specular light 구현

--- src/main.cpp
+++ src/main.cpp
@@ -139,6 +139,7 @@ int main()
         objectProgram.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         objectProgram.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
         objectProgram.setVec3("lightPos", lightPos);
+        objectProgram.setVec3("viewPos", camera.Position);
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 model = glm::mat4(1.0f);
