71a316fdf53cf65c3ac02b53d99a2f85cd9ef558
DESKTOP-9T5OK38\dosss <dosss2726@gmail.com>
2024-01-20 16:51:50 +0900

    포스트 프로세싱

--- shader/fragmentScreen.fs
+++ shader/fragmentScreen.fs
@@ -4,10 +4,36 @@ out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
+const float offset = 1.0 / 300.0;  
 
 void main()
-{ 
-    FragColor = texture(screenTexture, TexCoords);
-    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
-    FragColor = vec4(average, average, average, 1.0);
+{
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // 좌측 상단
+        vec2( 0.0f,    offset), // 중앙 상단
+        vec2( offset,  offset), // 우측 상단
+        vec2(-offset,  0.0f),   // 좌측 중앙
+        vec2( 0.0f,    0.0f),   // 정중앙
+        vec2( offset,  0.0f),   // 우측 중앙
+        vec2(-offset, -offset), // 좌측 하단
+        vec2( 0.0f,   -offset), // 중앙 하단
+        vec2( offset, -offset)  // 우측 하단   
+    );
+
+float kernel[9] = float[](
+    1,1,1,
+    1,-8,1,
+    1,1,1
+);
+    
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
+    
+    FragColor = vec4(col, 1.0);
 }
